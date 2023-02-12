f41a123584c1689daa6f306980fe44db78044369
peter_alfie <alfie1104@gmail.com>
2023-02-13 08:50:09 +0900

    feat(app): using the push token and sending push notifications

--- App.js
+++ App.js
@@ -1,6 +1,6 @@
 import { useEffect } from "react";
 import { StatusBar } from "expo-status-bar";
-import { Button, StyleSheet, Text, View } from "react-native";
+import { Alert, Button, StyleSheet, View, Platform } from "react-native";
 import * as Notifications from "expo-notifications";
 
 Notifications.setNotificationHandler({
@@ -12,6 +12,39 @@ Notifications.setNotificationHandler({
 });
 
 export default function App() {
+  useEffect(() => {
+    async function configurePushNotification() {
+      const { status: existingStatus } =
+        await Notifications.getPermissionsAsync();
+      let finalStatus = existingStatus;
+
+      if (existingStatus !== "granted") {
+        const { status } = await Notifications.requestPermissionsAsync();
+        finalStatus = status;
+      }
+
+      if (finalStatus !== "granted") {
+        Alert.alert(
+          "Permission required",
+          "Push notifications need the appropriate permissions."
+        );
+        return;
+      }
+
+      const pushTokenData = await Notifications.getExpoPushTokenAsync();
+      console.log(pushTokenData);
+
+      if (Platform.OS === "android") {
+        Notifications.setNotificationChannelAsync("default", {
+          name: "default",
+          importance: Notifications.AndroidImportance.DEFAULT,
+        });
+      }
+    }
+
+    configurePushNotification();
+  }, []);
+
   useEffect(() => {
     const subscription1 = Notifications.addNotificationReceivedListener(
       (notification) => {
@@ -37,8 +70,8 @@ export default function App() {
     };
   }, []);
 
-  async function scheduleNotificationHandler() {
-    await Notifications.scheduleNotificationAsync({
+  function scheduleNotificationHandler() {
+    Notifications.scheduleNotificationAsync({
       content: {
         title: "My first local notification",
         body: "This is the body of the notification.",
@@ -52,13 +85,29 @@ export default function App() {
     });
   }
 
+  function sendPushNotificationHandler() {
+    fetch(`https://exp.host/--/api/v2/push/send`, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        to: "ExponentPushToken[xxxxxxxxxxxxxxxxx]",
+        title: "Test - sent from a device!",
+        body: "This is a test!",
+      }),
+    });
+  }
+
   return (
     <View style={styles.container}>
       <Button
         title="Schedule Notification"
-        onPress={async () => {
-          await scheduleNotificationHandler();
-        }}
+        onPress={scheduleNotificationHandler}
+      />
+      <Button
+        title="Send Push Notification"
+        onPress={sendPushNotificationHandler}
       />
       <StatusBar style="auto" />
     </View>
